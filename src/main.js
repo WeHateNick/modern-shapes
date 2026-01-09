b2b08c25f77f6c8b57c9ef8118f9858dce4000ba
Nick Ocampo <nick@wehatenick.com>
2026-01-09 11:44:08 -0800

    feat: content and styles

--- src/main.js
+++ src/main.js
@@ -1,24 +1,32 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import modernShapesLogo from '/images/modern-shapes-logo.svg';
+import { setupCounter } from './counter.js';
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <div class="container">
+    <div class="title">
+      <img src="${modernShapesLogo}" class="logo" alt="Modern Shapes" />
+      <h2>MOMENTS WITH OTHERS MATTER</h2>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <div class="platforms">
+      <div class="platform spotify">
+        <h2>Listen on Spotify</h2>
+        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/album/03KcJx1kYvAMtJ6Iism3OL?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      </div>
+      <div class="platform apple">
+        <h2>Listen on Apple Music</h2>
+        <iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="450" style="width:100%;max-width:660px;overflow:hidden;background:transparent;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/moments-with-others-matter/1864091636"></iframe>
+      </div>
+      <div class="platform amazon">
+        <h2>Listen on Amazon Music</h2>
+        <iframe id='AmazonMusicEmbedB0GCCZZSPJ' src='https://music.amazon.com/embed/B0GCCZZSPJ/?id=yT4vUQhPJX&marketplaceId=ATVPDKIKX0DER&musicTerritory=US' width='100%' height='352px' frameBorder='0' style='border-radius:20px;max-width:'></iframe>
+      </div>
+      <div class="platform YouTube Music">
+        <h2>Listen on Apple Music</h2>
+        <a class="button" href="https://music.youtube.com/channel/UCaqB6IerUHT0sDT-_6HNffg?si=O4eJ2Ci522f2BV_Y" target="_blank">YouTube Music</a>
+      </div>
+    </div>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector('#counter'));
