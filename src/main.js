7fa460b1f0a89bfc70d10a588fc9ff5573a40fbf
Nick Ocampo <nick@wehatenick.com>
2026-01-09 12:23:26 -0800

    fix: button sizes

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ document.querySelector('#app').innerHTML = `
         <h2>Listen on Spotify</h2>
         <a class="button spotify" href="https://open.spotify.com/album/03KcJx1kYvAMtJ6Iism3OL?si=fdZTJSRTTUWuqpyRiIft3w" target="_blank">
           <div class="icon-separator">
-            <span>MOMENTS WITH OTHERS MATTER on Spotify</span>
+            <span>MOMENTS WITH OTHERS MATTER</span>
             <img class="icon" src="${spotifyIcon}" alt="Spotify Logo" />
           </div>
         </a>
@@ -30,7 +30,7 @@ document.querySelector('#app').innerHTML = `
         <h2>Listen on Apple Music</h2>
         <a class="button apple" href="https://music.apple.com/us/album/moments-with-others-matter/1864091636" target="_blank">
           <div class="icon-separator">
-            <span>MOMENTS WITH OTHERS MATTER on Apple Music</span>
+            <span>MOMENTS WITH OTHERS MATTER</span>
             <img class="icon" src="${appleMusicIcon}" alt="Apple Music Logo" />
           </div>
         </a>
@@ -39,7 +39,7 @@ document.querySelector('#app').innerHTML = `
         <h2>Listen on Amazon Music</h2>
         <a class="button amazon" href="https://music.amazon.com/albums/B0GCCZZSPJ?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_hgfb1IAgMtZ5mfQxU76mhiX55" target="_blank">
           <div class="icon-separator">
-            <span>MOMENTS WITH OTHERS MATTER on Amazon Music</span>
+            <span>MOMENTS WITH OTHERS MATTER</span>
             <img class="icon" src="${amazonMusicIcon}" alt="Amazon Music Logo" />
           </div>
         </a>
@@ -48,7 +48,7 @@ document.querySelector('#app').innerHTML = `
         <h2>Listen on YouTube Music</h2>
         <a class="button apple" href="https://music.youtube.com/playlist?list=OLAK5uy_ntruUhVlHjor2lzPXJbhIgqIrjDv6BDTs&si=UARYG-j0mdIdgvqy" target="_blank">
           <div class="icon-separator">
-            <span>MOMENTS WITH OTHERS MATTER on YouTube Music</span>
+            <span>MOMENTS WITH OTHERS MATTER</span>
             <img class="icon" src="${youtubeMusicIcon}" alt="YouTube Music Logo" />
           </div>
         </a>
